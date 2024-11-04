0132cf92a157bcce2ae8741a257cc010bbf682ce
Mohsin Hassan <mohsinhassan008@gmail.com>
2024-11-04 19:00:58 +0500

    made design changes and fixes

--- js/main.js
+++ js/main.js
@@ -77,13 +77,14 @@
 
     // Header carousel
     $(".header-carousel").owlCarousel({
-        autoplay: false,
-        smartSpeed: 1500,
+        autoplay: true, // Set to true for autoplay
+        autoplayTimeout: 6000, // Duration between slides
+        smartSpeed: 2500,
         items: 1,
         dots: false,
         loop: true,
-        nav : true,
-        navText : [
+        nav: true,
+        navText: [
             '<i class="bi bi-chevron-left"></i>',
             '<i class="bi bi-chevron-right"></i>'
         ]
